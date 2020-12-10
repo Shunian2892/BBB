8b05c5be16ab4a07cf64d67366f8ed0d82ba7e41
Nicholas <nkw.leroux@student.avans.nl>
2020-12-10 17:02:12 +0100

    [Fix] HelpPopUp dismiss/ok button

--- app/src/main/java/com/example/bbb/boundaryLayer/ui/HelpPopUp.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/ui/HelpPopUp.java
@@ -1,14 +1,10 @@
 package com.example.bbb.boundaryLayer.ui;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +14,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.bbb.R;
-import com.example.bbb.boundaryLayer.launcher.MainActivity;
 
 public class HelpPopUp extends DialogFragment {
 
@@ -27,10 +22,14 @@ public class HelpPopUp extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.pop_up_help, null));
+        View myview = inflater.inflate(R.layout.pop_up_help, null);
+        builder.setView(myview);
 
         AlertDialog alertDialog = builder.create();
         alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button button = myview.findViewById(R.id.buttonHelp);
+        button.setOnClickListener(view -> alertDialog.dismiss());
 
         return alertDialog;
     }

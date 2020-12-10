2176c1e268c2a5b06cda90c31b09f50d33b0068a
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-12-10 16:23:56 +0100

    [Bug] ok button in help popup bug

--- app/src/main/java/com/example/bbb/boundaryLayer/launcher/MainActivity.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/launcher/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.bbb.boundaryLayer.launcher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.bbb.R;
+import com.example.bbb.boundaryLayer.ui.HelpPopUp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment = new HelpPopUp();
+                dialogFragment.show(getSupportFragmentManager(), "JOEJOE");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bbb/boundaryLayer/ui/HelpPopUp.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/ui/HelpPopUp.java
@@ -1,4 +1,37 @@
 package com.example.bbb.boundaryLayer.ui;
 
-public class HelpPopUp {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.bbb.R;
+import com.example.bbb.boundaryLayer.launcher.MainActivity;
+
+public class HelpPopUp extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.pop_up_help, null));
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return alertDialog;
+    }
 }

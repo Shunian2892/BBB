e6b0d4a3c7d570fab8e42e911e9caca4cbfc4d3f
Nicholas <nkw.leroux@student.avans.nl>
2020-12-14 14:50:36 +0100

    [ADD] Testing with sharedPrefs

--- app/src/main/java/com/example/bbb/boundaryLayer/launcher/MainActivity.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/launcher/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.bbb.boundaryLayer.launcher;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
-
-import android.app.AlertDialog;
-import android.app.Application;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.bbb.R;
 import com.example.bbb.boundaryLayer.ui.HelpPopUp;
 import com.example.bbb.boundaryLayer.ui.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private FragmentManager fragmentManager;
+
+    private final BroadcastReceiver networkReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                mapFragment.invalidateMapView();
+            } catch (NullPointerException e) {
+                // lazy handling of an improbable NPE
+            }
+        }
+    };
+
     private MapFragment mapFragment;
 
     @Override
@@ -26,18 +36,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button = findViewById(R.id.button);
-//        button.setOnClickListener(view -> {
-//            DialogFragment dialogFragment = new HelpPopUp();
-//            dialogFragment.show(getSupportFragmentManager(), "JOEJOE");
-//        });
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            DialogFragment dialogFragment = new HelpPopUp();
+            dialogFragment.show(getSupportFragmentManager(), "JOEJOE");
+        });
 
-        fragmentManager = getSupportFragmentManager();
-        if(fragmentManager.findFragmentById(R.id.main_fragment) == null){
-            mapFragment = new MapFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.main_fragment) == null) {
+            mapFragment = MapFragment.newInstance();
             fragmentManager.beginTransaction().add(R.id.main_fragment, mapFragment).commit();
         } else {
             mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.main_fragment);
         }
+
+        registerReceiver(networkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(networkReceiver);
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/bbb/boundaryLayer/ui/MapFragment.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/ui/MapFragment.java
@@ -2,6 +2,7 @@ package com.example.bbb.boundaryLayer.ui;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -12,7 +13,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,23 +20,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.bbb.R;
+import com.example.bbb.controlLayer.gps.OpenStreetMaps;
+
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-import com.example.bbb.R;
-import com.example.bbb.controlLayer.gps.OpenStreetMaps;
 
 public class MapFragment extends Fragment {
+
+    private static final String PREFS_NAME = "MapFragment";
+    private static final String PREFS_TILE_SOURCE = "tilesource";
+    private static final String PREFS_LATITUDE_STRING = "latitudeString";
+    private static final String PREFS_LONGITUDE_STRING = "longitudeString";
+    private static final String PREFS_ORIENTATION = "orientation";
+    private static final String PREFS_ZOOM_LEVEL_DOUBLE = "zoomLevelDouble";
+
+    private SharedPreferences mPrefs;
     private Context fragmentContext;
     private IMapController mapController;
     private MapView map;
     private OpenStreetMaps osm;
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
 
     @Nullable
     @Override
@@ -45,17 +56,17 @@ public class MapFragment extends Fragment {
         Configuration.getInstance().load(fragmentContext, PreferenceManager.getDefaultSharedPreferences(fragmentContext));
 
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+        map = view.findViewById(R.id.map_view);
 
-        map = (MapView) view.findViewById(R.id.map_view);
         map.setUseDataConnection(true);
         map.setTileSource(TileSourceFactory.MAPNIK);
-
+        map.setDestroyMode(false);
         mapController = map.getController();
-        mapController.setZoom(14);
+        mapController.setZoom(20);
 
         //Check for Location permission
-        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.getActivity(), new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION}, 44);
@@ -71,16 +82,51 @@ public class MapFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //trying to load in last location with sharedPrefs.
+/*        final Context context = this.getActivity();
+
+        mPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        final float zoomLevel = mPrefs.getFloat(PREFS_ZOOM_LEVEL_DOUBLE, 1);
+        map.getController().setZoom(zoomLevel);
+        final float orientation = mPrefs.getFloat(PREFS_ORIENTATION, 0);
+        map.setMapOrientation(orientation, false);
+        final String latitudeString = mPrefs.getString(PREFS_LATITUDE_STRING, "1.0");
+        final String longitudeString = mPrefs.getString(PREFS_LONGITUDE_STRING, "1.0");
+        final double latitude = Double.valueOf(latitudeString);
+        final double longitude = Double.valueOf(longitudeString);
+
+        GeoPoint startPosition = new GeoPoint(latitude, longitude);
+        map.setExpectedCenter(startPosition);
+        getLocation(startPosition);*/
+    }
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        getLocation();
+//        getLocation(null);
+        getLocation(new GeoPoint(51.9225, 4.47917));
 
         osm.drawRoute(map);
     }
 
-    public void getLocation() {
+
+    public void getLocation(GeoPoint geoPoint) {
+
         LocationManager locationManager = (LocationManager) fragmentContext.getSystemService(Context.LOCATION_SERVICE);
+
+        //Not needed. Only if use savedPrefs
+        if (geoPoint != null) {
+            mapController.setCenter(geoPoint);
+            Marker startMarker = new Marker(map);
+            startMarker.setPosition(geoPoint);
+            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            startMarker.setVisible(true);
+            map.getOverlays().add(startMarker);
+        }
 
         LocationListener locationListener = new LocationListener() {
             @Override
@@ -94,8 +140,38 @@ public class MapFragment extends Fragment {
             }
         };
 
-        if(ContextCompat.checkSelfPermission(fragmentContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, locationListener);
+        if (ContextCompat.checkSelfPermission(fragmentContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        //save the current location with sharedPrefs
+/*        final SharedPreferences.Editor edit = mPrefs.edit();
+        edit.putString(PREFS_TILE_SOURCE, map.getTileProvider().getTileSource().name());
+        edit.putFloat(PREFS_ORIENTATION, map.getMapOrientation());
+        edit.putString(PREFS_LATITUDE_STRING, String.valueOf(map.getMapCenter().getLatitude()));
+        edit.putString(PREFS_LONGITUDE_STRING, String.valueOf(map.getMapCenter().getLongitude()));
+        edit.putFloat(PREFS_ZOOM_LEVEL_DOUBLE, (float) map.getZoomLevelDouble());
+        edit.apply();*/
+
+        map.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(fragmentContext, PreferenceManager.getDefaultSharedPreferences(fragmentContext));
+    }
+
+    public void invalidateMapView() {
+        if (map != null) {
+            map.invalidate();
         }
     }
+
+
 }

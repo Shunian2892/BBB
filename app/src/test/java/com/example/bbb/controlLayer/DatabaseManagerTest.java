cc9e8da263ba481750fbfb0ca8e1a976cfb968a3
Nicholas <nkw.leroux@student.avans.nl>
2020-12-29 15:52:59 +0100

    [ADD] Made UnitTest for DatabaseManager

---
+++ app/src/test/java/com/example/bbb/controlLayer/DatabaseManagerTest.java
@@ -0,0 +1,52 @@
+package com.example.bbb.controlLayer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseManagerTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void initDatabase() {
+    }
+
+    @Test
+    public void readJson() {
+    }
+
+    @Test
+    public void loadJSONFromFile() {
+    }
+
+    @Test
+    public void testQueries() {
+    }
+
+    @Test
+    public void getPOIs() {
+    }
+
+    @Test
+    public void getRoutes() {
+    }
+
+    @Test
+    public void getPOIsFromRoute() {
+    }
+
+    @Test
+    public void addWalkedRoute() {
+    }
+
+    @Test
+    public void getWalkedRoutes() {
+    }
+
+    @Test
+    public void searchLocation() {
+    }
+}

3255c5047d0126dddbe256b25339369c57695087
Nathalie Seen <n.seen@student.avans.nl>
2021-01-04 15:02:31 +0100

    [added] white text for detail landscape, url for videos detailview

--- app/src/main/java/com/example/bbb/boundaryLayer/ui/VideoFragment.java
+++ app/src/main/java/com/example/bbb/boundaryLayer/ui/VideoFragment.java
@@ -1,6 +1,7 @@
 package com.example.bbb.boundaryLayer.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,31 +10,44 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bbb.R;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
+import org.jetbrains.annotations.NotNull;
+
 public class VideoFragment extends Fragment {
     private YouTubePlayerView youTubePlayerView;
+    private float timer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_video, container, false);
-
+        UIViewModel viewModel = new ViewModelProvider(getActivity()).get(UIViewModel.class);
         youTubePlayerView = view.findViewById(R.id.youtubePlayer);
         getLifecycle().addObserver(youTubePlayerView);
 
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
-            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                String videoId = "1FJHYqE0RDg";
-                youTubePlayer.loadVideo(videoId, 0);
+            public void onReady(@NotNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                String videoId = viewModel.getSelectedPOI().getValue().VideoURL;
+                youTubePlayer.loadVideo(videoId,timer );
+            }
+
+            @Override
+            public void onCurrentSecond(@NotNull YouTubePlayer youTubePlayer, float second) {
+                super.onCurrentSecond(youTubePlayer, second);
+                timer = second;
             }
         });
+
         return view;
+
     }
 
 }
